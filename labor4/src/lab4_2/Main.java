c15cb30c97d85a4f2f07e16befeb17ff48a7377b
alpargabos1 <gabosalpar0304@gmail.com>
2021-03-08 19:15:43 +0200

    labor 4_2 kesz

--- labor4/src/lab4_2/Main.java
+++ labor4/src/lab4_2/Main.java
@@ -7,7 +7,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         ArrayList<Customer> customers = new ArrayList<>();
+        customers = readFromFile("lab4_2_input.txt");
+        for(Customer item : customers){
+            System.out.println(item.toString());
+        }
     }
     public static ArrayList<Customer> readFromFile(String fileName){
         ArrayList<Customer> customers = new ArrayList<>();
@@ -20,8 +25,15 @@ public class Main {
         while(scanner.hasNextLine()){
             String line = scanner.nextLine();
             String[] items = line.split(",");
-            if(items[0] == "Customer"){
-
+            int counter = customers.size();
+            if(items[0].trim().equals("Customer")){
+                customers.add(new Customer(items[1].trim(),items[2].trim()));
+                //System.out.printf("\n%s %s accounts: \n",customers.get(counter).getFirstName(),customers.get(counter).getLastName());
+            }
+            if(items[0].trim().equals("Account")){
+                customers.get(counter - 1).addAccount(new BankAccount(items[1].trim()));
+                customers.get(counter - 1).getAccount(items[1].trim()).deposit(Double.parseDouble(items[2].trim()));
+                //System.out.println(customers.get(counter - 1).toString());
             }
 
         }
